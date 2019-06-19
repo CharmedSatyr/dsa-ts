19d6d295f3b2ef001ef28f3fb863c99e151fbc23
Joseph <z@charmed.tech>
2019-06-19 00:31:34 -0700

    Add `next` property to Node class; detail types

--- src/__tests__/node.test.ts
+++ src/__tests__/node.test.ts
@@ -1,3 +1,5 @@
+import { INode } from '../declarations/interfaces';
+import { TAN } from '../declarations/types';
 import Node from '../node';
 
 import { random } from 'faker';
@@ -5,18 +7,27 @@ const { alphaNumeric } = random;
 
 describe('`Node` class', () => {
   it('can instantiate a node without data', () => {
-    const node: Node = new Node();
+    const node: INode = new Node();
     expect(node).toBeDefined();
   });
 
   it('has a `data` property with the value of `null`', () => {
-    const node: Node = new Node();
+    const node: INode = new Node();
     expect(node.data).toBeNull();
   });
 
   it('can instantiate a node with data that is stored in a `data` property', () => {
-    const data: string | number = alphaNumeric();
-    const node: Node = new Node(data);
+    const data: TAN = alphaNumeric();
+    const node: INode = new Node(data);
     expect(node.data).toBe(data);
+  });
+
+  it('can instantiate a node with a `next` property of `null` or a `Node`', () => {
+    const data: TAN = alphaNumeric();
+    const node: INode = new Node(data);
+    const next: INode = new Node(data);
+    node.next = next;
+    expect(node.data).toBe(data);
+    expect(node.next).toEqual(next);
   });
 });
