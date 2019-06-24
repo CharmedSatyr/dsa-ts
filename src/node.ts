900c5a5ca261ac4d7997073e5d0d49c90950e8a2
Joseph <z@charmed.tech>
2019-06-24 11:42:50 -0700

    Add optional node properties for data type support

--- src/node.ts
+++ src/node.ts
@@ -8,9 +8,17 @@ import { TData, TEntry } from './declarations/types';
 class Node {
   public data: TData;
   public next?: TEntry;
+  public left?: TEntry;
+  public right?: TEntry;
   constructor(data: TData = null) {
     this.data = data;
+
+    // Stack/Queue
     this.next = null;
+
+    // Binary tree/BST
+    this.left = null;
+    this.right = null;
   }
 }
 
